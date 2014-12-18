12eb2c2f192844ad0701f1b07e60830640d27537
Jamis Johnson <jamismanwaring@gmail.com>
2014-12-18 16:29:07 -0500

    simple spark app running locally including run shell script

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+spark-submit --master local[4] sentiment.py
--- sentiment.py
+++ sentiment.py
@@ -1,3 +1,5 @@
+from pyspark import SparkContext
+
 from collections import defaultdict
 from pprint import pprint
 
@@ -14,3 +16,12 @@ with open('effectwordnet/goldStandard.tff') as f:
             # add each word to the sentiment dictionary
             if sentiment != 'Null':
                 sentimentdict[word] = sentiment[0]
+
+if __name__ == "__main__":
+    #conf = SparkConf().setAppName(appName).setMasterpmaster)
+    sc = SparkContext(appName='TweetSentiment')
+
+    textFile = sc.textFile('data/tweets.sample')
+    tweets = textFile.filter(lambda line: bool(line) and '@' == line[0])
+    #print "NUMBER OF TWEETS: %i" % tweets.count()
+    pprint(tweets.collect())
